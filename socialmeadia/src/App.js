6fcc1cfbc8a1806044fec8b3240f9f2edda3775a
Your Namekshai-7 <akshai2537@gmail.com>
2023-06-20 16:19:10 +0530

    trycatch commit

--- socialmeadia/src/App.js
+++ socialmeadia/src/App.js
@@ -1,4 +1,4 @@
-import {Link,Routes ,Route} from 'react-router-dom';
+import {Routes ,Route, useNavigate} from 'react-router-dom';
 import About from "./About";
 import Header from "./Header";
 import Nav from "./Nav";
@@ -7,60 +7,69 @@ import PostPage from "./PostPage";
 import Footer from "./Footer";
 import Home from "./Home";
 import Missing from "./Missing";
-import Post from "./Post";
-import PostLayout from './PostLayout';
 import { useEffect, useState } from 'react';
 import {format} from "date-fns"
- 
+import api from "./api/posts"
+
+
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'My First Post',
-      datetime: "July 01, 2022 11:17:35 AM",
-      body:"Made a Video About Tesla Q1 results"
-    },
-    {
-      id: 2,
-      title: 'My Second Post',
-      datetime: "July 01, 2022 11:17:35 AM",
-      body:"I attended a Defi blockchain event"
-    },
-    {
-      id: 3,
-      title: 'My Third Post',
-      datetime: "July 01, 2022 11:17:35 AM",
-      body:"Web3 globel summit next week"
-    },
-    {
-      id: 4,
-      title: 'My Fourth Post',
-      datetime: "July 01, 2022 11:17:35 AM",
-      body:"ETH will outperform BTC"
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState(''); 
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts')
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error:${err.message}`);
+        }
+      }
+    }
+    fetchPosts();
+  },[])
+
+
   useEffect(() => {
     const filteredResults = posts.filter( post =>  ((post.body).toLowerCase()).includes(search.toLowerCase()) || ((post.title).toLowerCase()).includes(search.toLowerCase())) ;
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigate('/');
+    } catch (err) {
+      if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error:${err.message}`);
+        }
+    }
+   
   }
   const handleDelete = (id) => {
     const postsList = posts.filter(post => post.id !== id);
     setPosts(postsList);
+    navigate('/');
   }
   return (
     <div className="App">
@@ -79,13 +88,11 @@ function App() {
             postBody={postBody}
             setPostBody={setPostBody}
           /> } />
-          <Route path=":id" element={<PostPage posts={posts} handleDelete={handleDelete() } />} />
+          <Route path=":id" element={<PostPage posts={posts}  handleDelete={handleDelete} />} />
         </Route>
-        
       <Route path='about' element={ <About/> } />
       <Route path='*' element={ <Missing/> } />
       </Routes>
-      
     <Footer/>
     </div>
   );
